12275af19cac956c1115573eea9025f93a45ddfa
Egor <egorkakuzka@gmail.com>
2026-04-27 16:36:45 +0300

    Восстановление репозитория после повреждения

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная - Мой сайт</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon"> <!-- добавим позже -->
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    <main>
+        <h1>Добро пожаловать!</h1>
+        <p>Это главная страница сайта, созданного в рамках лабораторной работы.</p>
+    </main>
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
